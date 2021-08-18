7e48718b45bf1475e20832cbfaed40e83406e16e
Reneator <reneator@googlemail.com>
2021-08-18 10:30:18 +0200

    moved cross origins to controller

--- src/main/java/de/rene/Car/Configurator/CarConfigurationController.java
+++ src/main/java/de/rene/Car/Configurator/CarConfigurationController.java
@@ -1,11 +1,13 @@
 package de.rene.Car.Configurator;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class CarConfigurationController {
 
