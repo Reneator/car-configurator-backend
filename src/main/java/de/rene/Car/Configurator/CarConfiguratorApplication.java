0ce137524c620bd6f2d95be522e273d92bb7ce27
Reneator <reneator@googlemail.com>
2021-08-18 09:55:13 +0200

    added swagger + added tomcat7

--- src/main/java/de/rene/Car/Configurator/CarConfiguratorApplication.java
+++ src/main/java/de/rene/Car/Configurator/CarConfiguratorApplication.java
@@ -4,11 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
-public class CarConfiguratorApplication {
+public class CarConfiguratorApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarConfiguratorApplication.class, args);
